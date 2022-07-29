98d62026329bd1d73c12e8d346d35257ebf48b54
Sharnu Ravindra Narsagol <sharanuvishaln@gmail.com>
2022-07-29 21:04:37 +0530

    get ag-grid data from nest-app

--- apps/nest-app/src/app/app.service.ts
+++ apps/nest-app/src/app/app.service.ts
@@ -2,7 +2,12 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getData(): { message: string } {
-    return { message: 'Welcome to nest-app!' };
+  demoData = [
+    { make: 'Tata', model: 'Celica', price: 35000 },
+    { make: 'Tata', model: 'Mondeo', price: 32000 },
+    { make: 'Tata', model: 'Boxster', price: 72000 }
+];
+  getData() {
+    return this.demoData;
   }
 }
